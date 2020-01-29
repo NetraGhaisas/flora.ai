77193506b9cb32d7d0d3914ea971f3fa0e8ad79f
NetraGhaisas <netraghaisas@gmail.com>
2020-01-29 11:41:42 +0530

    Tab bar works

--- lib/startup.dart
+++ lib/startup.dart
@@ -11,56 +11,120 @@ class StartupPage extends StatefulWidget {
 }
 
 class _StartupPageState extends State<StartupPage> {
-  final List<Tab> tab = <Tab> [
-    Tab(text: 'HISTORY',),
-    Tab(text: 'CLICK',),
+  final List<Tab> tab = <Tab>[
+    Tab(
+      text: 'HISTORY',
+    ),
+    Tab(
+      text: 'CLICK',
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    var screenHeight = size.height;
+    var screenWidth = size.width;
     return Scaffold(
       appBar: _getAppBar(),
       body: SizedBox(
-        height: 60,
-      child: DefaultTabController(
-          length: tab.length,
-          initialIndex: 1,
-          child: Scaffold(
-            appBar: PreferredSize(
-            preferredSize: Size.fromHeight(100),
-            child: new SafeArea(child: Container(
-              // color: Theme.of(context).primaryColor,
-              child: Column(children: <Widget>[
-                new TabBar(tabs: tab,
-                unselectedLabelColor: Theme.of(context).primaryColor,
-                dragStartBehavior: DragStartBehavior.down,
-                labelPadding: EdgeInsets.symmetric(vertical: 5),
-                labelColor: Theme.of(context).scaffoldBackgroundColor,
-                indicatorSize: TabBarIndicatorSize.tab,
-                indicator: new BubbleTabIndicator(
-                indicatorHeight: 25.0,
-                padding: EdgeInsets.symmetric(horizontal: 0),
-                indicatorColor: Theme.of(context).accentColor,
-
-                tabBarIndicatorSize: TabBarIndicatorSize.tab,
-              ))],),
-            )),
+          height: screenHeight,
+          child: DefaultTabController(
+            length: tab.length,
+            initialIndex: 1,
+            child: Scaffold(
+                appBar: PreferredSize(
+                  preferredSize: Size.fromHeight(100),
+                  child: new SafeArea(
+                      child: Container(
+                    // color: Theme.of(context).primaryColor,
+                    child: Column(
+                      children: <Widget>[
+                        new TabBar(
+                            tabs: tab,
+                            unselectedLabelColor:
+                                Theme.of(context).primaryColor,
+                            dragStartBehavior: DragStartBehavior.down,
+                            labelPadding: EdgeInsets.symmetric(vertical: 5),
+                            labelColor:
+                                Theme.of(context).scaffoldBackgroundColor,
+                            indicatorSize: TabBarIndicatorSize.tab,
+                            indicator: new BubbleTabIndicator(
+                              indicatorHeight: 25.0,
+                              padding: EdgeInsets.symmetric(horizontal: 0),
+                              indicatorColor: Theme.of(context).accentColor,
+                              tabBarIndicatorSize: TabBarIndicatorSize.tab,
+                            ))
+                      ],
+                    ),
+                  )),
+                ),
+                // appBar: AppBar(bottom: TabBar(tabs:tab,),title: Text('flora test',style:TextStyle(color:Colors.black)),),
+                body: TabBarView(
+                  children: [
+                    Text('hello',style:TextStyle(color:Colors.black)),
+                    // Icon(Icons.directions_car,color:Colors.black),
+                    Icon(Icons.directions_transit),
+                  ],
+                )),
           )),
-)
-      ),
     );
   }
 
   AppBar _getAppBar() {
     return AppBar(
       backgroundColor: Theme.of(context).primaryColor,
-      title: Row(
-        children: [
-          Image.asset('images/app icon.png'),
-          Padding(padding: EdgeInsets.only(right: 10)),
-          Text('flora.ai', 
-          style: TextStyle(fontFamily: 'Less Sans', fontSize: 20, color: Theme.of(context).accentColor))]
-          ),
+      title: Row(children: [
+        Image.asset('images/app icon.png'),
+        Padding(padding: EdgeInsets.only(right: 10)),
+        Text('flora.ai',
+            style: TextStyle(
+                fontFamily: 'Less Sans',
+                fontSize: 20,
+                color: Theme.of(context).accentColor))
+      ]),
     );
   }
+}
+
+Container _getHistory(BuildContext context) {
+  // Widget history = Container(
+  //   child:Text('Hello', style: TextStyle(color: Colors.black)),
+  // );
+  
+  Size size = MediaQuery.of(context).size;
+    var screenHeight = size.height;
+    var screenWidth = size.width;
+  Widget history = new Container(
+    height: 350,
+    width: screenWidth,
+    color: Theme.of(context).accentColor,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 10, left: 20, right: 20),
+          child: Text('HISTORY',
+              style: TextStyle(color: Theme.of(context).accentColor)),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: Text("hello",
+              textAlign: TextAlign.justify,
+              style: TextStyle(color: Colors.black, fontSize: 14)),
+        ),
+      ],
+    ),
+  );
+  return history;
+}
+
+Container _getClick(BuildContext context) {
+  Widget click = Container(
+    child: Text('Click!'),
+  );
+  return click;
 }
